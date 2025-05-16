d7ad7ef1f40aa19073b26323a262f3168c17386b
Erik Hollensbe <git@hollensbe.org>
2025-05-16 11:44:58 -0700

    refactors
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/bin/buckled/main.rs
+++ src/bin/buckled/main.rs
@@ -1,12 +1,6 @@
-use buckle::grpc::status_server::StatusServer;
-use buckle::server::Server as BuckleServer;
-use tonic::transport::Server;
+use buckle::server::Server;
 
 #[tokio::main]
-pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    Server::builder()
-        .add_service(StatusServer::new(BuckleServer))
-        .serve("[::]:5001".parse()?)
-        .await?;
-    Ok(())
+pub async fn main() -> Result<(), anyhow::Error> {
+    Server::default().start("[::]:5001".parse()?).await
 }
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,21 @@
-use tonic::{Request, Response, Result};
+use std::net::SocketAddr;
 
-use crate::grpc::status_server::Status;
+use crate::grpc::status_server::{Status, StatusServer};
+use tonic::{transport::Server as TransportServer, Request, Response, Result};
+
+#[derive(Debug, Default, Clone)]
 pub struct Server;
+
+impl Server {
+    pub async fn start(&self, addr: SocketAddr) -> Result<(), anyhow::Error> {
+        TransportServer::builder()
+            .add_service(StatusServer::new(self.clone()))
+            .serve(addr)
+            .await?;
+
+        Ok(())
+    }
+}
 
 #[tonic::async_trait]
 impl Status for Server {
