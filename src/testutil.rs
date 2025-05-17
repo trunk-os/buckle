36f36bffd182d9e698e52142a034282674dcf88e
Erik Hollensbe <git@hollensbe.org>
2025-05-17 00:25:43 -0700

    testing pools appear now; they were before it just wasn't being tested
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/testutil.rs
+++ src/testutil.rs
@@ -5,6 +5,8 @@ use std::{net::SocketAddr, time::Duration};
 use tokio::net::TcpListener;
 use tonic::transport::Channel;
 
+const BUCKLE_TEST_ZPOOL_PREFIX: &str = "buckle-test";
+
 pub(crate) async fn find_listener() -> Result<SocketAddr> {
     for x in 3000..32767 {
         let addr: SocketAddr = format!("0.0.0.0:{}", x).parse()?;
@@ -46,7 +48,7 @@ pub(crate) fn create_zpool(name: &str) -> Result<String> {
     std::process::Command::new("zpool")
         .args(vec![
             "create",
-            &format!("buckle-test-{}", name),
+            &format!("{}-{}", BUCKLE_TEST_ZPOOL_PREFIX, name),
             path.to_str().unwrap(),
         ])
         .stdout(std::io::stdout())
@@ -58,18 +60,53 @@ pub(crate) fn create_zpool(name: &str) -> Result<String> {
 #[cfg(feature = "zfs")]
 pub(crate) fn destroy_zpool(name: &str, file: &str) -> Result<()> {
     std::process::Command::new("zpool")
-        .args(vec!["destroy", "-f", &format!("buckle-test-{}", name)])
+        .args(vec![
+            "destroy",
+            "-f",
+            &format!("{}-{}", BUCKLE_TEST_ZPOOL_PREFIX, name),
+        ])
         .status()?;
     Ok(std::fs::remove_file(&file)?)
 }
 
 #[cfg(feature = "zfs")]
+pub(crate) fn list_zpools() -> Result<Vec<String>> {
+    let out = String::from_utf8(
+        std::process::Command::new("zpool")
+            .args(vec!["list"])
+            .output()?
+            .stdout,
+    )?;
+    let lines = out.split('\n');
+
+    let mut ret = Vec::new();
+
+    for line in lines.skip(1) {
+        let mut name = String::new();
+        for ch in line.chars() {
+            if ch != ' ' {
+                name.push(ch)
+            } else {
+                break;
+            }
+        }
+        ret.push(name);
+    }
+
+    Ok(ret)
+}
+
+#[cfg(feature = "zfs")]
 mod tests {
-    use super::{create_zpool, destroy_zpool};
+    use super::{create_zpool, destroy_zpool, list_zpools, BUCKLE_TEST_ZPOOL_PREFIX};
+
     #[test]
     fn create_remove_zfs() {
         let file = create_zpool("testutil-test").unwrap();
         assert!(file.len() > 0);
+        assert!(list_zpools()
+            .unwrap()
+            .contains(&format!("{}-testutil-test", BUCKLE_TEST_ZPOOL_PREFIX)));
         destroy_zpool("testutil-test", &file).unwrap();
         assert!(!std::fs::exists(file).unwrap())
     }
